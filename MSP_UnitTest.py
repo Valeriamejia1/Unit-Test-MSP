ef4759055e61af7e3d6e92ccaae7503ada7442f5
valeriamejia1 <valeria.mejia@ayahealthcare.com>
2024-05-07 14:49:49 -0600

    Test

--- MSP_UnitTest.py
+++ MSP_UnitTest.py
@@ -6,8 +6,8 @@ class TestExcel(unittest.TestCase):
         self.assertEqual(original_cell.value, new_cell.value, f"Cell value at {location} is different")
 
     def test_MVHS(self):
-        original_file_path = "C:\\Python tools\\Unit Test MSP\\Original Files\\MVHS - Invoice-4102271-ORIG.xlsx"
-        new_file_path = "C:\\Python tools\\Unit Test MSP\\New files\\MVHS - Wynn Hopsital\\Invoice-4102271.xlsx"
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\MVHS - Invoice-4102271-ORIG.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\MVHS - Wynn Hopsital\\Invoice-4102271.xlsx"
 
         original_wb = load_workbook(filename=original_file_path)
         new_wb = load_workbook(filename=new_file_path)
@@ -37,8 +37,8 @@ class TestExcel(unittest.TestCase):
             self.fail("\n".join(differences_found))
 
     def test_ANT(self):
-        original_file_path = "C:\\Python tools\\Unit Test MSP\\Original Files\\ANTHC-23-D-426757 ORIG.xlsx"
-        new_file_path = "C:\\Python tools\\Unit Test MSP\\New files\\Alaska Native Trival\\ANTHC-23-D-426757.xlsx"
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\ANTHC-23-D-426757 ORIG.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\Alaska Native Trival\\ANTHC-23-D-426757.xlsx"
 
         original_wb = load_workbook(filename=original_file_path)
         new_wb = load_workbook(filename=new_file_path)
@@ -68,8 +68,8 @@ class TestExcel(unittest.TestCase):
             self.fail("\n".join(differences_found))
 
     def test_EHS(self):
-        original_file_path = "C:\\Python tools\\Unit Test MSP\\Original Files\\Eliot 10174064 - OR – Surg Tech ORIG.xlsx"
-        new_file_path = "C:\\Python tools\\Unit Test MSP\\New files\\Elliot Health System\\10174064 - OR – Surg Tech.xlsx"
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\Eliot 10174064 - OR – Surg Tech ORIG.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\Elliot Health System\\10174064 - OR – Surg Tech.xlsx"
 
         original_wb = load_workbook(filename=original_file_path)
         new_wb = load_workbook(filename=new_file_path)
@@ -99,8 +99,8 @@ class TestExcel(unittest.TestCase):
             self.fail("\n".join(differences_found))
 
     # def test_UWH(self):
-    #     original_file_path = "C:\\Python tools\\Unit Test MSP\\Original Files\\UWH Invoice 4212785 - HMC ORIG.xlsx"
-    #     new_file_path = "C:\\Python tools\\Unit Test MSP\\New files\\Harborview Medical Center\\Invoice-4212785.xlsx"
+    #     original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\UWH Invoice 4212785 - HMC ORIG.xlsx"
+    #     new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\Harborview Medical Center\\Invoice-4212785.xlsx"
 
     #     original_wb = load_workbook(filename=original_file_path)
     #     new_wb = load_workbook(filename=new_file_path)
@@ -130,8 +130,8 @@ class TestExcel(unittest.TestCase):
     #         self.fail("\n".join(differences_found))
 
     def test_OSHU(self):
-        original_file_path = "C:\\Python tools\\Unit Test MSP\\Original Files\\OSHU Invoice 10176150 - Hillsboro Medical Center ORIG.xlsx"
-        new_file_path = "C:\\Python tools\\Unit Test MSP\\New files\\Oregon Health Science University\\Invoice 10176150 - Hillsboro Medical Center.xlsx"
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\OSHU Invoice 10176150 - Hillsboro Medical Center ORIG.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\Oregon Health Science University\\Invoice 10176150 - Hillsboro Medical Center.xlsx"
 
         original_wb = load_workbook(filename=original_file_path)
         new_wb = load_workbook(filename=new_file_path)
@@ -161,8 +161,8 @@ class TestExcel(unittest.TestCase):
             self.fail("\n".join(differences_found))
 
     def test_SGMC(self):
-        original_file_path = "C:\\Python tools\\Unit Test MSP\\Original Files\\SGMC Invoice - #4185042 Cath Lab ORIG.xlsx"
-        new_file_path = "C:\\Python tools\\Unit Test MSP\\New files\\South Georgia Health System\\Invoice - #4185042 Cath Lab.xlsx"
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\SGMC Invoice - #4185042 Cath Lab ORIG.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\South Georgia Health System\\Invoice - #4185042 Cath Lab.xlsx"
 
         original_wb = load_workbook(filename=original_file_path)
         new_wb = load_workbook(filename=new_file_path)
@@ -192,8 +192,8 @@ class TestExcel(unittest.TestCase):
             self.fail("\n".join(differences_found))
 
     def test_PMH(self):
-        original_file_path = "C:\\Python tools\\Unit Test MSP\\Original Files\\PMH 10181025 - Manchester Memorial Hospital - ER ORIG.xlsx"
-        new_file_path = "C:\\Python tools\\Unit Test MSP\\New files\\Eastern Connecticut Health Network Float Pool\\10181025 - Manchester Memorial Hospital - ER.xlsx"
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\PMH 10181025 - Manchester Memorial Hospital - ER ORIG.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\Eastern Connecticut Health Network Float Pool\\10181025 - Manchester Memorial Hospital - ER.xlsx"
 
         original_wb = load_workbook(filename=original_file_path)
         new_wb = load_workbook(filename=new_file_path)
@@ -223,8 +223,8 @@ class TestExcel(unittest.TestCase):
             self.fail("\n".join(differences_found))
 
     def test_UWA(self):
-        original_file_path = "C:\\Python tools\\Unit Test MSP\\Original Files\\UWA Invoice-4185038 ORIG.xlsx"
-        new_file_path = "C:\\Python tools\\Unit Test MSP\\New files\\University of Washington Medical Center\\Invoice-4185038.xlsx"
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\UWA Invoice-4185038 ORIG.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\University of Washington Medical Center\\Invoice-4185038.xlsx"
 
         original_wb = load_workbook(filename=original_file_path)
         new_wb = load_workbook(filename=new_file_path)
