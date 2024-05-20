7f362fa4972439c314d2e41f516a955d6da0ef56
Mateo Bolanos <Mateo.Bolanos@ayahealthcare.com>
2024-05-20 16:26:30 -0600

    Test MSPDEV_Mateo

--- MSP_UnitTest.py
+++ MSP_UnitTest.py
@@ -98,36 +98,36 @@ class TestExcel(unittest.TestCase):
         if differences_found:
             self.fail("\n".join(differences_found))
 
-    def test_UWH(self):
-        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\UWH Invoice 4212785 - HMC ORIG.xlsx"
-        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\Harborview Medical Center\\Invoice-4212785.xlsx"
+    # def test_UWH(self):
+    #     original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\UWH Invoice 4212785 - HMC ORIG.xlsx"
+    #     new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\Harborview Medical Center\\Invoice-4212785.xlsx"
 
-        original_wb = load_workbook(filename=original_file_path)
-        new_wb = load_workbook(filename=new_file_path)
+    #     original_wb = load_workbook(filename=original_file_path)
+    #     new_wb = load_workbook(filename=new_file_path)
 
-        original_sheet_names = original_wb.sheetnames
-        new_sheet_names = new_wb.sheetnames
+    #     original_sheet_names = original_wb.sheetnames
+    #     new_sheet_names = new_wb.sheetnames
 
-        self.assertEqual(original_sheet_names, new_sheet_names, "Number of sheets are not equal")
+    #     self.assertEqual(original_sheet_names, new_sheet_names, "Number of sheets are not equal")
 
-        differences_found = []
-        for sheet_name in original_sheet_names:
-            original_sheet = original_wb[sheet_name]
-            new_sheet = new_wb[sheet_name]
+    #     differences_found = []
+    #     for sheet_name in original_sheet_names:
+    #         original_sheet = original_wb[sheet_name]
+    #         new_sheet = new_wb[sheet_name]
 
-            original_rows = original_sheet.iter_rows()
-            new_rows = new_sheet.iter_rows()
+    #         original_rows = original_sheet.iter_rows()
+    #         new_rows = new_sheet.iter_rows()
 
-            for (row_idx, original_row), new_row in zip(enumerate(original_rows), new_rows):
-                for col_idx, (original_cell, new_cell) in enumerate(zip(original_row, new_row)):
-                    location = f"sheet '{sheet_name}', row {row_idx+1}, column {col_idx+1}"
-                    try:
-                        self.assertCellEqual(original_cell, new_cell, location)
-                    except AssertionError as e:
-                        differences_found.append(str(e))
+    #         for (row_idx, original_row), new_row in zip(enumerate(original_rows), new_rows):
+    #             for col_idx, (original_cell, new_cell) in enumerate(zip(original_row, new_row)):
+    #                 location = f"sheet '{sheet_name}', row {row_idx+1}, column {col_idx+1}"
+    #                 try:
+    #                     self.assertCellEqual(original_cell, new_cell, location)
+    #                 except AssertionError as e:
+    #                     differences_found.append(str(e))
 
-        if differences_found:
-            self.fail("\n".join(differences_found))
+    #     if differences_found:
+    #         self.fail("\n".join(differences_found))
 
     def test_OSHU(self):
         original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\OSHU Invoice 10176150 - Hillsboro Medical Center ORIG.xlsx"
@@ -222,36 +222,36 @@ class TestExcel(unittest.TestCase):
         if differences_found:
             self.fail("\n".join(differences_found))
 
-    def test_UWA(self):
-        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\UWA Invoice-4185038 ORIG.xlsx"
-        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\University of Washington Medical Center\\Invoice-4185038.xlsx"
+    # def test_UWA(self):
+    #     original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\UWA Invoice-4185038 ORIG.xlsx"
+    #     new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\University of Washington Medical Center\\Invoice-4185038.xlsx"
 
-        original_wb = load_workbook(filename=original_file_path)
-        new_wb = load_workbook(filename=new_file_path)
+    #     original_wb = load_workbook(filename=original_file_path)
+    #     new_wb = load_workbook(filename=new_file_path)
 
-        original_sheet_names = original_wb.sheetnames
-        new_sheet_names = new_wb.sheetnames
+    #     original_sheet_names = original_wb.sheetnames
+    #     new_sheet_names = new_wb.sheetnames
 
-        self.assertEqual(original_sheet_names, new_sheet_names, "Number of sheets are not equal")
+    #     self.assertEqual(original_sheet_names, new_sheet_names, "Number of sheets are not equal")
 
-        differences_found = []
-        for sheet_name in original_sheet_names:
-            original_sheet = original_wb[sheet_name]
-            new_sheet = new_wb[sheet_name]
+    #     differences_found = []
+    #     for sheet_name in original_sheet_names:
+    #         original_sheet = original_wb[sheet_name]
+    #         new_sheet = new_wb[sheet_name]
 
-            original_rows = original_sheet.iter_rows()
-            new_rows = new_sheet.iter_rows()
+    #         original_rows = original_sheet.iter_rows()
+    #         new_rows = new_sheet.iter_rows()
 
-            for (row_idx, original_row), new_row in zip(enumerate(original_rows), new_rows):
-                for col_idx, (original_cell, new_cell) in enumerate(zip(original_row, new_row)):
-                    location = f"sheet '{sheet_name}', row {row_idx+1}, column {col_idx+1}"
-                    try:
-                        self.assertCellEqual(original_cell, new_cell, location)
-                    except AssertionError as e:
-                        differences_found.append(str(e))
+    #         for (row_idx, original_row), new_row in zip(enumerate(original_rows), new_rows):
+    #             for col_idx, (original_cell, new_cell) in enumerate(zip(original_row, new_row)):
+    #                 location = f"sheet '{sheet_name}', row {row_idx+1}, column {col_idx+1}"
+    #                 try:
+    #                     self.assertCellEqual(original_cell, new_cell, location)
+    #                 except AssertionError as e:
+    #                     differences_found.append(str(e))
 
-        if differences_found:
-            self.fail("\n".join(differences_found))
+    #     if differences_found:
+    #         self.fail("\n".join(differences_found))
 
     def test_CMH(self):
         original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\CMH Invoice-10182990-Smith, Sherri A-Nursing Residency ORIG.xlsx"
