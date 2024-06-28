2f0ca5f563a36e6df92724ff10d6de9d0f3f9fe2
valeriamejia1 <valeria.mejia@ayahealthcare.com>
2024-06-28 16:08:42 -0600

    ADD CSMC, DCH AND BCH

--- MSP_UnitTest.py
+++ MSP_UnitTest.py
@@ -281,6 +281,90 @@ class TestExcel(unittest.TestCase):
                     except AssertionError as e:
                         differences_found.append(str(e))
 
+    def test_BCH(self):
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\BCH 10201214 Mease Countryside Hospital.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\Bay Care\\10201214 Mease Countryside Hospital.xlsx"
+
+        original_wb = load_workbook(filename=original_file_path)
+        new_wb = load_workbook(filename=new_file_path)
+
+        original_sheet_names = original_wb.sheetnames
+        new_sheet_names = new_wb.sheetnames
+
+        self.assertEqual(original_sheet_names, new_sheet_names, "Number of sheets are not equal")
+
+        differences_found = []
+        for sheet_name in original_sheet_names:
+            original_sheet = original_wb[sheet_name]
+            new_sheet = new_wb[sheet_name]
+
+            original_rows = original_sheet.iter_rows()
+            new_rows = new_sheet.iter_rows()
+
+            for (row_idx, original_row), new_row in zip(enumerate(original_rows), new_rows):
+                for col_idx, (original_cell, new_cell) in enumerate(zip(original_row, new_row)):
+                    location = f"sheet '{sheet_name}', row {row_idx+1}, column {col_idx+1}"
+                    try:
+                        self.assertCellEqual(original_cell, new_cell, location)
+                    except AssertionError as e:
+                        differences_found.append(str(e))
+
+    def test_DCH(self):
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\DCH 10196613 DCH Regional Medical Center.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\DCH Health System\\10196613 DCH Regional Medical Center.xlsx"
+
+        original_wb = load_workbook(filename=original_file_path)
+        new_wb = load_workbook(filename=new_file_path)
+
+        original_sheet_names = original_wb.sheetnames
+        new_sheet_names = new_wb.sheetnames
+
+        self.assertEqual(original_sheet_names, new_sheet_names, "Number of sheets are not equal")
+
+        differences_found = []
+        for sheet_name in original_sheet_names:
+            original_sheet = original_wb[sheet_name]
+            new_sheet = new_wb[sheet_name]
+
+            original_rows = original_sheet.iter_rows()
+            new_rows = new_sheet.iter_rows()
+
+            for (row_idx, original_row), new_row in zip(enumerate(original_rows), new_rows):
+                for col_idx, (original_cell, new_cell) in enumerate(zip(original_row, new_row)):
+                    location = f"sheet '{sheet_name}', row {row_idx+1}, column {col_idx+1}"
+                    try:
+                        self.assertCellEqual(original_cell, new_cell, location)
+                    except AssertionError as e:
+                        differences_found.append(str(e))
+
+    def test_CSMC(self):
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\CSMC 10189919 Cedars-Sinai Medical Center.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\Cedars-Sinai Medical Center\\10189919 Cedars-Sinai Medical Center.xlsx"
+
+        original_wb = load_workbook(filename=original_file_path)
+        new_wb = load_workbook(filename=new_file_path)
+
+        original_sheet_names = original_wb.sheetnames
+        new_sheet_names = new_wb.sheetnames
+
+        self.assertEqual(original_sheet_names, new_sheet_names, "Number of sheets are not equal")
+
+        differences_found = []
+        for sheet_name in original_sheet_names:
+            original_sheet = original_wb[sheet_name]
+            new_sheet = new_wb[sheet_name]
+
+            original_rows = original_sheet.iter_rows()
+            new_rows = new_sheet.iter_rows()
+
+            for (row_idx, original_row), new_row in zip(enumerate(original_rows), new_rows):
+                for col_idx, (original_cell, new_cell) in enumerate(zip(original_row, new_row)):
+                    location = f"sheet '{sheet_name}', row {row_idx+1}, column {col_idx+1}"
+                    try:
+                        self.assertCellEqual(original_cell, new_cell, location)
+                    except AssertionError as e:
+                        differences_found.append(str(e))
+
         if differences_found:
             self.fail("\n".join(differences_found))
 
