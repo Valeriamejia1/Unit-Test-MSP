5c170835791adb93cbd9e997242ec6ae941f05c1
valeriamejia1 <valeria.mejia@ayahealthcare.com>
2024-07-12 16:31:52 -0600

    Review EIH, BCH, DCHS, CMH, EHS, CHN, SMH

--- MSP_UnitTest.py
+++ MSP_UnitTest.py
@@ -365,6 +365,63 @@ class TestExcel(unittest.TestCase):
                     except AssertionError as e:
                         differences_found.append(str(e))
 
+    def test_CHN(self):
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\CHN - Invoice - # 10223952 L&D CST NIGHTS.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\COmmunity Health CHN\\Invoice - # 10223952 L&D CST NIGHTS.xlsx"
+
+        original_wb = load_workbook(filename=original_file_path)
+        new_wb = load_workbook(filename=new_file_path)
+
+        original_sheet_names = original_wb.sheetnames
+        new_sheet_names = new_wb.sheetnames
+
+        self.assertEqual(original_sheet_names, new_sheet_names, "Number of sheets are not equal")
+
+        differences_found = []
+        for sheet_name in original_sheet_names:
+            original_sheet = original_wb[sheet_name]
+            new_sheet = new_wb[sheet_name]
+
+            original_rows = original_sheet.iter_rows()
+            new_rows = new_sheet.iter_rows()
+
+            for (row_idx, original_row), new_row in zip(enumerate(original_rows), new_rows):
+                for col_idx, (original_cell, new_cell) in enumerate(zip(original_row, new_row)):
+                    location = f"sheet '{sheet_name}', row {row_idx+1}, column {col_idx+1}"
+                    try:
+                        self.assertCellEqual(original_cell, new_cell, location)
+                    except AssertionError as e:
+                        differences_found.append(str(e))
+
+    def test_SMH(self):
+        original_file_path = "C:\\Python tools\\Unit-Test-MSP\\Original Files\\SMH - 10203328 - Sturdy Memorial Hospital - A - OR  PACU  ENDO.xlsx"
+        new_file_path = "C:\\Python tools\\Unit-Test-MSP\\New files\\Sturdy Memorial Hospital\\10203328 - Sturdy Memorial Hospital - A - OR  PACU  ENDO.xlsx"
+
+
+        original_wb = load_workbook(filename=original_file_path)
+        new_wb = load_workbook(filename=new_file_path)
+
+        original_sheet_names = original_wb.sheetnames
+        new_sheet_names = new_wb.sheetnames
+
+        self.assertEqual(original_sheet_names, new_sheet_names, "Number of sheets are not equal")
+
+        differences_found = []
+        for sheet_name in original_sheet_names:
+            original_sheet = original_wb[sheet_name]
+            new_sheet = new_wb[sheet_name]
+
+            original_rows = original_sheet.iter_rows()
+            new_rows = new_sheet.iter_rows()
+
+            for (row_idx, original_row), new_row in zip(enumerate(original_rows), new_rows):
+                for col_idx, (original_cell, new_cell) in enumerate(zip(original_row, new_row)):
+                    location = f"sheet '{sheet_name}', row {row_idx+1}, column {col_idx+1}"
+                    try:
+                        self.assertCellEqual(original_cell, new_cell, location)
+                    except AssertionError as e:
+                        differences_found.append(str(e))
+
         if differences_found:
             self.fail("\n".join(differences_found))
 
